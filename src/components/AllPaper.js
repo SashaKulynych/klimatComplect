c27c81c8529118e90e1029f76fdd4427cb9398b7
sergey-grishko <megusta970@gmail.com>
2018-05-05 11:43:53 +0300

    v

--- src/components/AllPaper.js
+++ src/components/AllPaper.js
@@ -48,9 +48,6 @@ class AllPaper extends Component {
         return (
             <div>
                 <Header/>
-                <div className="afterHeader">
-                    <img style={{width:"100%"}} src={require('./images/pictureCategory.png')} alt=""/>
-                </div>
                 <div className="container">
                     <p className="col-12 title text-center">Всі статті</p>
                     <div className="row mb-4">
--- src/components/Reference.js
+++ src/components/Reference.js
@@ -6,10 +6,18 @@ import './styles/text.css'
 
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
+import Slider from 'react-slick';
 
 class Reference extends Component {
 
     render() {
+        let settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
         return (
             <div>
                 <Header/>
@@ -17,6 +25,28 @@ class Reference extends Component {
                     <div className="textContainer container_wrap">
                         <p>Об’єкти, на яких успішно використовується наше обладнання - добре відомі та легко впізнавані будівлі по всій території України. Часто вони мають принципове значення у своїй галузі або значний обсяг виробництва, тож ми усвідомлюємо рівень відповідальності, що нам припадає. Кожен наш об’єкт – витвір мистецтва у дуже специфічному жанрі та водночас, високотехнологічна і точна система.</p>
                         <p>Ми пишаємося своїм доробком у технологічному прогресі України, довершеною естетикою та комфортом створюваних умов. І безперечно, своїми задоволеними клієнтами!</p>
+                    </div>
+                    <div className="col-lg-12 text-center" style={{paddingBottom:50}}>
+                        <Slider {...settings}>
+                            <div style={{display:"flex", justifyContent:"center"}}>
+                                <img src={require("./images/photo_2018-05-05_11-32-44.jpg")} style={{height:"500px", margin:"auto"}} className="img_us" alt=""/>
+                            </div>
+                            <div style={{display:"flex", justifyContent:"center"}}>
+                                <img src={require("./images/photo_2018-05-05_11-32-47.jpg")} style={{height:"500px", margin:"auto"}} className="img_us" alt=""/>
+                            </div>
+                            <div style={{display:"flex", justifyContent:"center"}}>
+                                <img src={require("./images/photo_2018-05-05_11-32-50.jpg")} style={{height:"500px", margin:"auto"}} className="img_us" alt=""/>
+                            </div>
+                            <div style={{display:"flex", justifyContent:"center"}}>
+                                <img src={require("./images/photo_2018-05-05_11-32-53.jpg")} style={{height:"500px", margin:"auto"}} className="img_us" alt=""/>
+                            </div>
+                            <div style={{display:"flex", justifyContent:"center"}}>
+                                <img src={require("./images/photo_2018-05-05_11-32-55.jpg")} style={{height:"500px", margin:"auto"}} className="img_us" alt=""/>
+                            </div>
+                            <div style={{display:"flex", justifyContent:"center"}}>
+                                <img src={require("./images/photo_2018-05-05_11-32-58.jpg")} style={{height:"500px", margin:"auto"}} className="img_us" alt=""/>
+                            </div>
+                        </Slider>
                     </div>
                 </div>
                 <Footer/>
